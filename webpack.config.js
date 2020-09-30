9b24ac7a18b8fd3283fa138c3de8f0d7942005ad
Mataeux Gauthier <mataeuxg@gmail.com>
2020-09-29 22:54:23 -0700

    Add react test

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const SRC_DIR = path.join(__dirname, '/client');
 const DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
+  mode: 'development',
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
@@ -13,11 +14,15 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
           presets: ['@babel/preset-env', '@babel/preset-react'],
         },
       },
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx', '.css'],
   },
 };
