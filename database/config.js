10fc1331c989398f8412110e3600f50cc7847c64
Mikhail <juliinstant88@gmail.com>
2020-10-17 13:07:52 -0400

    adding sql and cql

--- database/config.js
+++ database/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  host: 'mysql',
+  host: 'localhost',
   user: 'root',
   // if you are using the pairing stations, the user is `student`
-  password: 'mysql',
+  password: 'emma18',
   // if you are using the paring stations, the password is `student`
   // if you do not have a password, delete this key/value pair from
   // the object
---
+++ database/postgres-schema.sql
@@ -0,0 +1,60 @@
+DROP DATABASE IF EXISTS reviewssdc;
+
+CREATE DATABASE reviewssdc;
+
+\c reviewssdc;
+
+USE reviewssdc;
+
+CREATE TABLE IF NOT EXISTS restaurants (
+  id serial primary key,
+  name_of_restaurant varchar(50),
+  number_of_reviews int,
+  rating_overall decimal(2, 1),
+  rating_recent decimal(2, 1),
+  rating_food decimal(2, 1),
+  rating_service decimal(2, 1),
+  rating_ambience decimal(2, 1),
+  noise_level varchar(20),
+  would_recommend decimal(3, 2),
+  percent_five_star decimal(3, 2),
+  percent_four_star decimal(3, 2),
+  percent_three_star decimal(3, 2),
+  percent_two_star decimal(3, 2),
+  percent_one_star decimal(3, 2),
+  loved_for varchar(100),
+  filters varchar(100)
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  id serial primary key,
+  avatar varchar(100),
+  first_name varchar(20),
+  last_name varchar(20),
+  number_of_reviews int,
+  locale varchar(50)
+);
+
+CREATE TABLE IF NOT EXISTS review_list (
+  id serial primary key,
+  id_restaurants int,
+  avatar varchar(100),
+  first_name varchar(20),
+  last_name varchar(20),
+  number_of_reviews int,
+  locale varchar(50),
+  create_date_month varchar(50),
+  create_date_day varchar(50),
+  create_date_year varchar(50),
+  review_message varchar(1000),
+  rating_overall int,
+  rating_recent int,
+  rating_food int,
+  rating_service int,
+  rating_ambience int,
+  noise_level varchar(20),
+  would_recommend int,
+  loved_for varchar(150),
+  filters varchar(150)
+
+);
